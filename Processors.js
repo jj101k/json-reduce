3eae67cde1f2a20154d7220cfc84c4603fa8bf1d
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-01-14 01:11:43 +0000

    Use bigger buffers for more testing

--- Processors.js
+++ Processors.js
@@ -90,11 +90,11 @@ class Processors {
     }
 
     static referenceIn(contents) {
-        return child_process.execSync("gzip -c", {input: contents}).toString("base64")
+        return child_process.execSync("gzip -c", {input: contents, maxBuffer: 100_000_000}).toString("base64")
     }
 
     static referenceOut(contents) {
-        return child_process.execSync("gzip -cd", {input: Buffer.from(contents, "base64"), encoding: "utf-8"})
+        return child_process.execSync("gzip -cd", {input: Buffer.from(contents, "base64"), encoding: "utf-8", maxBuffer: 100_000_000})
     }
 }
 
