96df418cbcdf3a0188af8385161652b74b20792f
Jim Driscoll <jim.a.driscoll@gmail.com>
2023-01-22 12:08:42 +0000

    Simplify the shorten to avoid actual decode/encode

--- src/Processor/Local.ts
+++ src/Processor/Local.ts
@@ -1,9 +1,14 @@
 export abstract class Local {
     static debug = false
 
+    /**
+     *
+     * @param contents
+     * @returns
+     */
     static shortenIfNeeded(contents: string) {
         if (contents.match(/^[\[{]\r?\n/s)) {
-            return JSON.stringify(JSON.parse(contents))
+            return contents.replace(/\n\s*"((?:[^"]*\\.)*)":\s+/g, `"$1"`).replace(/\r?\n\s*/g, "")
         } else {
             return contents
         }
